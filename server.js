1fd0315732d2741a4e53bcd0ea4dc3c72018903d
Webfire-Marketing <management@webfire-marketing.com>
2026-04-02 21:15:22 +0200

    Fix basic cancel url

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/checkout-basic', async (req, res) => {
         proration_behavior: 'create_prorations',
       },
       success_url: 'https://webfire-marketing.com',
-      cancel_url: 'https://webfire-marketing.com',
+      cancel_url: 'https://webfire-marketing.com/?checkout_cancelled=1',
     });
 
     res.redirect(session.url);
